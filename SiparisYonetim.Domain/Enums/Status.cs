2fd314b130c715e7d19a9acb591e319c53a11cf2
Tolga YILDIZ <tolgayildiz91@hotmail.com.tr>
2023-10-30 00:05:08 +0300

    proje oluşturuldu

---
+++ SiparisYonetim.Domain/Enums/Status.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiparisYonetim.Domain.Enums
+{
+    public enum Status
+    {
+        Active = 1,
+        Deactive = 2,
+        Deleted = 3
+    }
+}
