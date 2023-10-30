aa8675e4185b34aadb9b0274fabbc7a85d4447a4
Tolga Yildiz <Tolga.Yildiz@suratkargo.com.tr>
2023-10-30 10:05:33 +0300

    identity alt yapısı kuruldu

--- SiparisYonetim.Domain/Entities/Concrete/AppRole.cs
+++ SiparisYonetim.Domain/Entities/Concrete/AppRole.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,7 @@ using System.Threading.Tasks;
 
 namespace SiparisYonetim.Domain.Entities.Concrete
 {
-    public class Manager:AppUser
+    public class AppRole:IdentityRole<Guid>
     {
-        public int BranchID { get; set; }
     }
 }
---
+++ SiparisYonetim.Domain/Entities/Concrete/Branch.cs
@@ -0,0 +1,29 @@
+﻿using SiparisYonetim.Domain.Entities.Abstract;
+using SiparisYonetim.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiparisYonetim.Domain.Entities.Concrete
+{
+    public class Branch : IEntity<int>, IBaseEntity
+    {
+        public int ID { get; set; }
+        public string BranchName { get; set; }
+
+
+
+        public virtual List<Manager> Managers { get; set; }
+        public Guid ManagerId { get; set; }
+
+
+        public string CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string? ModifiedBy { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+        public Status Status { get; set; }
+
+    }
+}
